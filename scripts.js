d351a24484b46e8b0aaf2680ac626ab8db1e6d01
Alexander Bobykin <birmudo@gmail.com>
2023-05-23 23:29:33 +0600

    Completed task code

--- scripts.js
+++ scripts.js
@@ -3,9 +3,14 @@ const myDialog = document.querySelector('#my-dialog')
 const closeDialogBtn = document.querySelector('#close-dialog-btn')
 
 showDialogBtn.addEventListener('click', () => {
-  myDialog.showModal()
+  myDialog.show()
 })
 
 closeDialogBtn.addEventListener('click', () => {
   myDialog.close()
+})
+
+document.addEventListener('click', (e) => {
+  if (!e.target.matches('#show-dialog-btn') && !e.target.matches('path'))
+    myDialog.close()
 })
